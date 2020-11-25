d28dbbe3b168faa8f02cbfe5c37338a41c8fac38
Gorgi Georgievski <ggeorgie@redhat.com>
2020-11-25 19:32:16 +0100

    add main.js

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,3 +1,11 @@
+const core = require('@actions/core')
 
-console.log("Hello World");
+const firstGreeting = core.getInput('first-greeting')
+const secondGreeting = core.getInput('second-greeting')
+const thirdGreeting = core.getInput('third-greeting')
 
+console.log(`Hello ${firstGreeting}`)
+console.log(`Hello ${secondGreeting}`)
+if(thirdGreeting) {
+    console.log(`Hello ${thirdGreeting}`)
+}
