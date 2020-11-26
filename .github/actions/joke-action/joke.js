4fcf0dc457ebca8de8a6d8b6c12c2d24f41df3b9
Gorgi Georgievski <ggeorgie@redhat.com>
2020-11-26 16:03:24 +0100

    joke-action

---
+++ .github/actions/joke-action/joke.js
@@ -0,0 +1,18 @@
+const  request  = require("request-promise")
+
+const options = {
+    method:  "GET",
+    url: "http://icanhazdadjoke.com/",
+    headers: {
+        Accept: "application/json",
+        "User-Agent": "Writing JavaScript action GitHub Learning Lab course.  Visit lab.github.com or to contact us."
+    },
+    json: true
+}
+
+async function getJoke() {
+    const req = await request(options);
+    return req.joke
+}
+
+module.exports = getJoke;
